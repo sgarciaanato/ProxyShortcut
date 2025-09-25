72d88223a4c747a4b8afc766cbfe52345bc8f710
Samuel <samuel.garcia@walmart.com>
2025-09-25 13:22:35 -0300

    Initial Commit

---
+++ ProxyShortcut/ProxyShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxyShortcutApp.swift
+//  ProxyShortcut
+//
+//  Created by Samuel Garcia on 25-09-25.
+//
+
+import SwiftUI
+
+@main
+struct ProxyShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
